94c42075365f76e2643c97f0e44edce6faef1aed
Jordan Bautista <jordanbau@gmail.com>
2017-04-24 16:32:30 -0700

    commented

--- mimicStream.js
+++ mimicStream.js
@@ -1,3 +1,6 @@
+//function pushes a random 1-100 number and date every second into array
+//initializing a new variable to the return value of this function will provide you with the "stream" of data
+
 function createStream () {
     function ranNum() {
         return Math.floor(Math.random()*99);;
