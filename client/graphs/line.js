ad0f0884083e7fcf8ba5a9cbe01f6263e7a36612
Jordan Bautista <jordanbau@gmail.com>
2017-05-01 13:43:21 -0700

    removing / appending new graph in function

--- client/graphs/line.js
+++ client/graphs/line.js
@@ -1,48 +1,60 @@
 
-var socket = io.connect();
+let socket = io.connect();
 
-let queue = [];
-let data = [];
-let count = 0;
+let margin = { top: 20, right: 20, bottom: 20, left: 20 };
+let width = 700 - margin.left - margin.right;
+let height = 500 - margin.top - margin.bottom;
 
-var margin = { top: 20, right: 20, bottom: 20, left: 20 };
-var width = 700 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
+let currData = [];
+let svg = d3.select('.chart')
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('class', 'mount')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-var svg = d3.select('.chart')
-  .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-//-----------------------------------------------------------
 socket.on('sendStreamData', (allData) => {
-    console.log('all data: ', allData);
+  
+  if(allData.length > currData.length) {
+    currData = allData;
+    drawViz(allData)
+  };
 
-    queue = queue.concat(allData);
+})
 
-    console.log('queue', queue);
-    console.log('data: ', data); 
+function drawViz(allData) {
 
-    if (allData.length >= 50) {
-      allData = allData.slice(-49);
-    }
-if(allData) {
+  d3.select('svg').remove();
 
-  var xScale = d3.scaleLinear()
-    // .domain([0, 200])
+  svg = d3.select('.chart')
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('class', 'mount')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  if (allData.length >= 50) allData = allData.slice(-49);
+
+  let xScale = d3.scaleLinear()
     .domain([
       d3.min(allData, d => d.xScale),
       Math.max(50, d3.max(allData, d => d.xScale))
     ])
     .range([0, width]);
+
+  let yScale = d3.scaleLinear()
+    .domain([0, 35])
+    .range([height, 0]);
+
+  let line = d3.line()
+    .x(d => xScale(d.xScale))
+    .y(d => yScale(d.yScale))
+
   svg
     .append('g')
     .attr('transform', `translate(0, ${height})`)
-
-  svg
-    .select('g')
     .call(d3.axisBottom(xScale).ticks(10));
 
   // Add the text label for the x axis
@@ -53,35 +65,24 @@ if(allData) {
     .style('font-size', '13px')
     .text('');
 
-  var yScale = d3.scaleLinear()
-    .domain([0, 35])
-    .range([height, 0]);
-
   svg
     .append('g')
-    .attr('class', 'yAxis');
-  
-  svg.select('.yAxis')
+    .attr('class', 'yAxis')
     .call(d3.axisLeft(yScale).ticks(10));
 
   svg.append("text")
-        .attr("transform", "rotate(0)")
-        .attr("y",-10)
-        .attr("x", -40)
-        .attr("dy", "1em")
-        .attr('class', 'yLabel')
-        .style("text-anchor", "end")
-        .style('font-family', 'sans-serif')
-        .style('font-size', '13px')
-        .text("");
+    .attr("transform", "rotate(0)")
+    .attr("y", -10)
+    .attr("x", -40)
+    .attr("dy", "1em")
+    .attr('class', 'yLabel')
+    .style("text-anchor", "end")
+    .style('font-family', 'sans-serif')
+    .style('font-size', '13px')
+    .text('');
 
-  var line = d3.line()
-    .x(d => xScale(d.xScale))
-    .y(d => yScale(d.yScale))
-    //.curve(d3.curveCatmullRom.alpha(.5));
-
-  d3.selectAll('path.line').remove();
-  d3.selectAll('.dot').remove();
+  // d3.selectAll('path.line').remove();
+  // d3.selectAll('.dot').remove();
 
   svg
     .selectAll('.line')
@@ -93,13 +94,12 @@ if(allData) {
     .style('stroke', '#5176B6')
     .style('stroke-width', 1)
     .style('fill', 'none')
-    .style('stroke-linejoin','round');
+    .style('stroke-linejoin', 'round');
 
-
-svg.selectAll('.dot')
-  .data(allData)
-  .enter()
-  .append('circle')
+  svg.selectAll('.dot')
+    .data(allData)
+    .enter()
+    .append('circle')
     .attr('class', 'dot')
     .attr('cx', line.x())
     .attr('cy', line.y())
@@ -108,17 +108,3 @@ svg.selectAll('.dot')
     .style('stroke-width', 1.5)
     .style('stroke', 'DodgerBlue');
 }
-
-})
-
-setInterval(() => {
-  if(queue.length > 1) {
-    data.push(queue[count]);
-    count++;
-  }
-}, 1000);
-
-
-
- 
-
