38e52ea1d065eb17c05ac04634316867b2c25078
Jordan Bautista <jordanbau@gmail.com>
2017-05-06 14:05:09 -0700

    rendering all color bars

---
+++ client/graphs/bar.js
@@ -0,0 +1,62 @@
+(function () {
+  let socket = io.connect();
+
+  //set initial SVG params
+  let margin = { top: 20, right: 20, bottom: 40, left: 60 };
+  let width = 700 - margin.left - margin.right;
+  let height = 500 - margin.top - margin.bottom;
+
+
+  //array to compare incoming data >> if data is the same, do not rerender
+  let currData = [];
+  let svg;
+
+  socket.on('sendBarData', (allData) => {
+
+    d3.select('svg').remove();
+
+    svg = d3.select('.chart')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+    let yScale = d3.scaleLinear()
+      .domain([0, 70])
+      .range([height, 0]);
+
+    let yAxis = d3.axisLeft(yScale);
+
+    svg.call(yAxis);
+
+    let xScale = d3.scaleBand()
+      .paddingOuter(.5)
+      .paddingInner(0.1)
+      .domain(allData.map(d => d.xScale))
+      .range([0, width]);
+
+    let xAxis = d3.axisBottom(xScale)
+      .ticks(5)
+      .tickSize(10)
+      .tickPadding(5);
+
+    svg
+      .append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(xAxis)
+      .selectAll('text')
+
+    svg
+      .selectAll('rect')
+      .data(allData)
+      .enter()
+      .append('rect')
+      .attr('x', d => xScale(d.xScale))
+      .attr('y', d => yScale(d.volume))
+      .attr('width', d => xScale.bandwidth())
+      .attr('height', d => height - yScale(d.volume))
+      .attr('fill', (d,i) => d.color[i]);
+  });
+
+})();
