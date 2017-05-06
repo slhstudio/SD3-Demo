36477ffcd793da35844b0840a02c9e44b2a29cdc
Jordan Bautista <jordanbau@gmail.com>
2017-05-06 16:12:11 -0700

    drawViz func

--- client/graphs/bar.js
+++ client/graphs/bar.js
@@ -12,56 +12,72 @@
   let svg;
 
   socket.on('sendBarData', (allData) => {
-    
-
-      });
-
-    function drawViz(allData) {
-      d3.select('svg').remove();
-
-      svg = d3.select('.chart')
-        .append('svg')
-        .attr('id', 'barSVG')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
-      let yScale = d3.scaleLinear()
-        .domain([0, 70])
-        .range([height, 0]);
-
-      let yAxis = d3.axisLeft(yScale);
-
-      svg.call(yAxis);
-
-      let xScale = d3.scaleBand()
-        .paddingOuter(.5)
-        .paddingInner(0.1)
-        .domain(allData.map(d => d.xScale))
-        .range([0, width]);
-
-      let xAxis = d3.axisBottom(xScale)
-        .ticks(5)
-        .tickSize(10)
-        .tickPadding(5);
-
-      svg
-        .append('g')
-        .attr('transform', `translate(0, ${height})`)
-        .call(xAxis)
-        .selectAll('text')
-
-      svg
-        .selectAll('rect')
-        .data(allData)
-        .enter()
-        .append('rect')
-        .attr('x', d => xScale(d.xScale))
-        .attr('y', d => yScale(d.volume))
-        .attr('width', d => xScale.bandwidth())
-        .attr('height', d => height - yScale(d.volume))
-        .attr('fill', (d, i) => d.color[i]);
+    let incomingData = isNewData(currData, allData);
+    if(incomingData === 'NEW_OBJ') {
+      currData = allData;
+      drawViz(allData);
     }
+  });
 
+  function drawViz(allData) {
+    d3.select('svg').remove();
+
+    svg = d3.select('.chart')
+      .append('svg')
+      .attr('id', 'barSVG')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+    let yScale = d3.scaleLinear()
+      .domain([0, 70])
+      .range([height, 0]);
+
+    let yAxis = d3.axisLeft(yScale);
+
+    svg.call(yAxis);
+
+    let xScale = d3.scaleBand()
+      .paddingOuter(.5)
+      .paddingInner(0.1)
+      .domain(allData.map(d => d.xScale))
+      .range([0, width]);
+
+    let xAxis = d3.axisBottom(xScale)
+      .ticks(5)
+      .tickSize(10)
+      .tickPadding(5);
+
+    svg
+      .append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(xAxis)
+      .selectAll('text')
+
+    svg
+      .selectAll('rect')
+      .data(allData)
+      .enter()
+      .append('rect')
+      .attr('x', d => xScale(d.xScale))
+      .attr('y', d => yScale(d.volume))
+      .attr('width', d => xScale.bandwidth())
+      .attr('height', d => height - yScale(d.volume))
+      .attr('fill', (d, i) => d.color[i]);
+  }
+
+  function isNewData(a, b) {
+    if (a.length !== b.length) { return 'NEW_OBJ' };
+
+    for (let i = 0; i < a.length; i += 1) {
+      if (a[i].xScale === b[i].xScale && a[i].volume !== b[i].volume) {
+        // reRenderNode(b[i]);
+        return 'NEW_VOL'
+      } else if (a[i].xScale !== b[i].xScale) {
+        return 'NEW_OBJ';
+      };
+    }
+    return 'OLD_DATA';
+  }
 })();
