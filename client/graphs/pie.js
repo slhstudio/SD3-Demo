5ea531114fe0dc5110f6714084b02a78e7771d9e
slhstudio <slhstudio@gmail.com>
2017-05-09 20:09:06 -0700

    still working on it

--- client/graphs/pie.js
+++ client/graphs/pie.js
@@ -3,8 +3,8 @@ let socket = io.connect();
 
 //set initial SVG params
 let margin = { top: 10, right: 10, bottom: 10, left: 10 };
-let width = 500 - margin.left - margin.right;
-let height = 500 - margin.top - margin.bottom;
+let width = 600 - margin.left - margin.right;
+let height = 600 - margin.top - margin.bottom;
 let radius = width / 2;
 
 // let currData = [];
@@ -18,18 +18,23 @@ drawViz(allData);
 function drawViz(allData) {
 d3.select('svg').remove();
 //color range
-var color = d3.scaleSequential(d3.interpolateGnBu(.5));///d3.scaleOrdinal()
-//.range(['#BBDEF8', '#98CAF9', '#64B5F6', '#42A5F5', '#2196F3']);
+// var color = d3.scaleOrdinal()//
+// .range(['#BBDEF8', '#98CAF9', '#64B5F6', '#42A5F5', '#2196F3']);
+
+var color = d3.scaleSequential(d3.interpolateSpectral)
+   // .range(['lightgreen', 'darkgreen']) // or use hex values
+   //.range([0,1])
+   .domain([0, 25]);
 
 // arc generator for pie
 var arc = d3.arc()
   .outerRadius(radius - 10)
-  .innerRadius(0);
+  .innerRadius(20);
 
 //
 var labelArc = d3.arc()
-  .outerRadius(radius - 50)
-  .innerRadius(radius - 50)
+  .outerRadius(radius + 15)
+  .innerRadius(radius - 5)
 
 //pie generator
 var pie = d3.pie()
@@ -55,7 +60,10 @@ var svg = d3.select('.chart')
   //append the path of the arc
   g.append('path')
     .attr('d', arc)
-    .style('fill', (d) => color(d.data.category))
+    .style('fill', (d, i) => {
+      console.log(d); 
+      return color(d.index);
+    })
     .style('stroke', '#fff');
     // .transition()
     // .ease(d3.easeLinear)
@@ -67,9 +75,20 @@ var svg = d3.select('.chart')
     // .transition()
     // .ease(d3.easeLinear)
     // .duration(2000)
-    .attr('transform', d => ('translate(' + labelArc.centroid(d) + ')'))
-    .attr('dy', '.35em')
-    .text((d) => d.data.category);
+    // .attr('transform', d => ('translate('  + labelArc.centroid(d) + ')')) //('translate(' + labelArc.centroid(d) + ')'))
+    // .attr('dy', '.2em')
+    // .text(d => d.data.category)
+
+    //	pie.append("text")
+	  .attr("transform", d => {
+    let midAngle = d.endAngle < Math.PI ? d.startAngle/2 + d.endAngle/2 : d.startAngle/2  + d.endAngle/2 + Math.PI ;
+      //let midAngle =  d.startAngle/2  + d.endAngle/2 + Math.PI ;
+
+	  	return "translate(" + labelArc.centroid(d)[0] + "," + labelArc.centroid(d)[1] + ") rotate(-90) rotate(" + (midAngle * 180/Math.PI) + ")"; })
+	  .attr("dy", ".35em")
+    .attr('font-size', '14px')
+	  .attr('text-anchor','middle')
+	  .text(d =>  d.data.category);
 
 
 //  function pieTween(b) {
