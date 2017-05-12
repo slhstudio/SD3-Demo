cfff7b792e4f3f75c8d3e4d8b555e2b8a3efcc56
Jordan Bautista <jordanbau@gmail.com>
2017-05-11 19:37:05 -0700

    modularized word funcs, no update

--- client/graphs/word-cloud.js
+++ client/graphs/word-cloud.js
@@ -8,22 +8,22 @@
     }
   });
 
- 
+
   //these values will change dynamically based on how many words are in freq;
-  let cachedSize = 5;  
+  let cachedSize = 5;
   let h = 200;
   let w = 500;
 
   //get data from socket and store it in freq
-  var freq = [{"text":"your","size": 10},{"text":"the","size": 20},{"text":"at","size": 10}];
+  var freq = [{ "text": "your", "size": 10 }, { "text": "the", "size": 20 }, { "text": "at", "size": 10 }];
 
-  function includes(word){
+  function includes(word) {
     return freq.some(obj => {
       return obj.text === word;
     })
   }
 
-  
+
   socket.on('send audioData', (data) => {
     console.log('DATA RECEIVED', Date.now());
     //if word is in freq arr, then add 1; if not add it
@@ -35,70 +35,98 @@
         }
         if (!includes(word)) {
           freq.push(
-            {text: word, size: customData.fontSize}
+            { text: word, size: customData.fontSize }
           )
         }
       })
     });
 
+    cloud()
+      .size([w, h])
+      .words(freq)
+      .padding(customData.padding)
+      .overflow(true)
+      .rotate(customData.rotate)
+      .font(customData.font)
+      .fontSize(function (d) { return d.size; })
+      .on("end", hasGrid)
+      .start();
+  })
 
+  let settings;
 
-  //--------------------CREATE GRAPH----------------------------------
-    //d3 version 3 way of adding color;
-    //let fillColor = d3.scale.category20b();
+  function hasGrid(words) {
+
+    if (!settings) settings = drawGrid(words);
+    drawCloud(settings, words);
+
+  }
+
+  function drawGrid(data) {
+
     let color = d3.scaleLinear()
       .domain(customData.colorDomain)
       .range(customData.colors);
 
     let fillColor = d3.scaleOrdinal(d3.schemeCategory20);
-   
-    function alterSize() {
-      if (freq.length >= cachedSize) {
-        h += 40;
-        w += 70;
-        cachedSize += 5;
-      }
-    }
-    alterSize();
 
-    console.log('WIDTH', w, 'HEIGHT', h);
-    console.log('LENGTH', freq.length);  
-    console.log('cache', cachedSize);  
+    d3.select("#wordCloud").remove()
 
-    cloud()
-      .size([w, h])
-      .words(freq) 
-      .padding(customData.padding)
-      .overflow(true)
-      .rotate(customData.rotate)      
-      .font(customData.font)
-      .fontSize(function(d) { return d.size; })
-      .on("end", drawCloud)
-      .start();
+    let svg = d3.select("#word-cloud").append("svg")
+      .attr('id', 'wordCloud')
+      .attr("width", w)
+      .attr("height", h)
+      .append("g")
+      .attr('id', 'g-container')
+      .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
 
-    function drawCloud(words) {
-      //remove so doesn't make multiple word clouds
-      d3.select("#wordCloud").remove()
-      
-      d3.select("#word-cloud").append("svg")
-          .attr('id', 'wordCloud')
-          .attr("width", w)
-          .attr("height", h)
-        .append("g")
-        .attr("transform", "translate(" + w/2 + "," + h/2 + ")")
-        .selectAll("text")
-          .data(words)
-          .enter().append("text")
-          .style("font-size", function(d) { return (d.size) + "px"; })
-          .style("font-family", customData.font)
-          .style("fill", function(d, i) { return color(i); })
-          .attr("text-anchor", "middle")
-          .attr("transform", function(d,i) {
-            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-          })
-        .text(function(d) { return d.text; });
+    let settings = {
+      svg,
+      color,
+      fillColor,
     }
 
-  })
+    return settings;
+  }
+
+  function drawCloud(settings, words) {
+    console.log('drawing cloud...');
+    //remove so doesn't make multiple word clouds
+    let svg = settings.svg;
+    let color = settings.color;
+    let fillColor = settings.fillColor;
+
+    d3.select('#g-container').selectAll('.text')
+      // .selectAll("text")
+      .data(words)
+      .enter().append("text")
+      .attr('class', 'text')
+      .style("font-size", function (d) { return (d.size) + "px"; })
+      .style("font-family", customData.font)
+      .style("fill", function (d, i) { return color(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function (d, i) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function (d) { return d.text; });
+  }
+
+
+  //--------------------CREATE GRAPH----------------------------------
+  //d3 version 3 way of adding color;
+  //let fillColor = d3.scale.category20b();
+
+  function alterSize() {
+    if (freq.length >= cachedSize) {
+      h += 40;
+      w += 70;
+      cachedSize += 5;
+    }
+  }
+  alterSize();
+
+  console.log('WIDTH', w, 'HEIGHT', h);
+  console.log('LENGTH', freq.length);
+  console.log('cache', cachedSize);
 
 })()
